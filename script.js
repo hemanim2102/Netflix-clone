d89674780e16655e20bd915b04823d498f42d736
Hemani Maurya <hemanimaurya16@gmail.com>
2024-01-31 19:41:38 +0530

    modify

--- script.js
+++ script.js
@@ -7,16 +7,37 @@ const api_key = "api_key=d9b8ceb521e6d5a18d56f66bad20676b";
 const img_url = "https://image.tmdb.org/t/p/w500";
 const banner_url = "https://image.tmdb.org/t/p/original/";
 
+const paths = {
+    fetchAllCategories: `${base_url}/genre/movie/list?${api_key}&language=en-US`,
+    fetchMovieList: (id) => `${base_url}/discover/movie?${api_key}&with_genres= ${id}`,
+}
+
+// function init() {
+//     fetch_and_build_AllSections();
+// }
+
+// function fetch_and_build_AllSections() {
+//     fetch(paths.fetchAllCategories)
+//         .then(res => res.json())
+//         .then(res => {
+//             const categories = res.genres;
+//             console.table(categories);
+//         })
+//         .catch(err => console.error(err));
+// }
+
+// init();
+
 //requesting for data
 
 const requests = {
-    Trending: `${base_url}/trending/all/week?${api_key}&language=en-US`,
     NetflixOriginals: `${base_url}/discover/tv?${api_key}&with_networks=213`,
-    ActionMovies: `${base_url}/discover/movie?${api_key}&with_genres= 28`,
-    ComedyMovies: `${base_url}/discover/movie?${api_key}&with_genres= 35`,
-    HorrorMovies: `${base_url}/discover/movie?${api_key}&with_genres= 27`,
-    RomanceMovies: `${base_url}/discover/movie?${api_key}&with_genres= 10749`,
-    Documentaries: `${base_url}/discover/movie?${api_key}&with_genres= 99`,
+    Trending: `${base_url}/trending/all/week?${api_key}&language=en-US`,
+    // ActionMovies: `${base_url}/discover/movie?${api_key}&with_genres= 28`,
+    // ComedyMovies: `${base_url}/discover/movie?${api_key}&with_genres= 35`,
+    // HorrorMovies: `${base_url}/discover/movie?${api_key}&with_genres= 27`,
+    // RomanceMovies: `${base_url}/discover/movie?${api_key}&with_genres= 10749`,
+    // Documentaries: `${base_url}/discover/movie?${api_key}&with_genres= 99`,
 };
 
 //to truncate long data
@@ -43,7 +64,7 @@ fetch(requests.NetflixOriginals)
     .then((res) => res.json())
 
     .then((data) => {
-        console.log(data.results);
+        // console.log(data.results);
 
         const setMovie =
             data.results[Math.floor(Math.random() * data.results.length - 1)];
@@ -53,11 +74,16 @@ fetch(requests.NetflixOriginals)
         var banner_description = document.getElementById("description");
 
         banner.style.backgroundImage = "url(" + banner_url + setMovie.backdrop_path + ")";
+        banner.style.backgroundRepeat = "no-repeat";
+        banner.style.backgroundSize = "100%";
+        banner.style.backgroundPosition = "top";
         banner_description.innerText = truncateString(setMovie.overview, 150);
         banner_title.innerText = setMovie.name;
     });
 
 //to display movie rows
+
+//netflix originals
 fetch(requests.NetflixOriginals)
     .then((res) => res.json())
     .then((data) => {
@@ -69,7 +95,7 @@ fetch(requests.NetflixOriginals)
 
         const title = document.createElement("h2");
         title.className = "row-title";
-        title.innerText = "NETFLIX ORIGINALS";
+        title.innerText = "Netflix Originals";
         row.appendChild(title);
 
         const row_posters = document.createElement("div");
@@ -77,7 +103,7 @@ fetch(requests.NetflixOriginals)
         row.appendChild(row_posters);
 
         data.results.forEach((movie) => {
-            console.log(movie);
+
             const poster = document.createElement("img");
             poster.className = "row-poster-img";
 
@@ -87,3 +113,83 @@ fetch(requests.NetflixOriginals)
             row_posters.appendChild(poster);
         });
     })
+
+//trending movies
+fetch(requests.Trending)
+    .then((res) => res.json())
+    .then((data) => {
+        const head_row = document.getElementById("head-row");
+        const row = document.createElement("div");
+        row.className = "row";
+        row.classList.add("Netflix_row");
+        head_row.appendChild(row);
+
+        const title = document.createElement("h2");
+        title.className = "row-title";
+        title.innerText = "Trending";
+        row.appendChild(title);
+
+        const row_posters = document.createElement("div");
+        row_posters.className = "row-posters";
+        row.appendChild(row_posters);
+
+        data.results.forEach((movie) => {
+
+            const poster = document.createElement("img");
+            poster.className = "row-poster-img";
+
+            var s1 = movie.id;
+            poster.id = s1;
+            poster.src = img_url + movie.poster_path;
+            row_posters.appendChild(poster);
+        });
+    })
+
+//for movie genres rows
+function fetch_And_Build_Section(fetchUrl, category) {
+    console.log(fetchUrl, category);
+
+    //to fetch the row data
+    fetch(fetchUrl)
+        .then(res => res.json())
+        .then(data => {
+            const head_row = document.getElementById("head-row");
+            const row = document.createElement("div");
+            row.className = "row";
+            row.classList.add("Netflix_row");
+            head_row.appendChild(row);
+
+            const title = document.createElement("h2");
+            title.className = "row-title";
+            title.innerText = category;
+            row.appendChild(title);
+
+            const row_posters = document.createElement("div");
+            row_posters.className = "row-posters";
+            row.appendChild(row_posters);
+
+            data.results.forEach((movie) => {
+
+                const poster = document.createElement("img");
+                poster.className = "row-poster-img";
+
+                var s2 = movie.id;
+                poster.id = s2;
+                poster.src = img_url + movie.backdrop_path;
+                row_posters.appendChild(poster);
+            });
+        })
+        .catch(err => console.error(err));
+}
+
+fetch_And_Build_Section(paths.fetchMovieList(28), "Action");
+fetch_And_Build_Section(paths.fetchMovieList(35), "Comedy");
+fetch_And_Build_Section(paths.fetchMovieList(27), "Horror");
+fetch_And_Build_Section(paths.fetchMovieList(53), "Thriller");
+fetch_And_Build_Section(paths.fetchMovieList(878), "Science Fiction");
+fetch_And_Build_Section(paths.fetchMovieList(10749), "Romance");
+fetch_And_Build_Section(paths.fetchMovieList(18), "Drama");
+fetch_And_Build_Section(paths.fetchMovieList(10751), "Family");
+fetch_And_Build_Section(paths.fetchMovieList(80), "Crime");
+fetch_And_Build_Section(paths.fetchMovieList(36), "History");
+fetch_And_Build_Section(paths.fetchMovieList(99), "Documentaries");
